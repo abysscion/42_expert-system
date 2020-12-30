84aff51edaffd78504a90b9ace3693b85c5c64c4
Borisov Alexandr <borisov.av.dev@gmail.com>
2020-12-30 19:39:16 +0300

    lil fix

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@ if [ "$OSTYPE" == "msys" -o "$OSTYPE" == "cygwin" -o "$OSTYPE" == "darwin"* ] ; 
 	echo -e "\e[37;2m[Compiling...]"
 	find * -name "*.java" > sources.txt
 	javac -sourcepath . @sources.txt
-	echo -e "\e[37m[Creating executable...]\e[2m"
-	#jar cvfm ExpertSystem.jar manifest.fm "src/*"
 	echo -e "\e[1;92m[Ok]\e[0m"
 else
 	echo "Can't recognize OSTYPE. Try to build manually."
